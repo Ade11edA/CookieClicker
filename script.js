ea7c7802dddbfc55ea97b3b1136f3dc67e80a08e
Ade11edA <150388135+Ade11edA@users.noreply.github.com>
2024-01-04 14:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector('img').addEventListener('click', add);
 let count = 0;
-function add(e){
+function add(){
     console.log("hi")
     count += 1;
     document.querySelector('p').innerHTML ="Clicks: " + count;
